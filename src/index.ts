f1e7e81a7fa8d7413d9b99c61034f9307e421e48
Andy Edwards <jedwards@fastmail.com>
2020-01-17 16:16:18 -0600

    fix: choose parser intelligently

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode'
 import jscodeshift, { Transform, Options } from 'jscodeshift'
+import findRoot from 'find-root'
 
 export default async function applyTransform(
   transform: Transform,
@@ -14,15 +15,32 @@ export default async function applyTransform(
   const selectionStart = editor.document.offsetAt(editor.selection.start)
   const selectionEnd = editor.document.offsetAt(editor.selection.end)
 
+  let parser = options ? options.parser : null
+  if (!parser) {
+    const tsMatch = /\.(tsx?)$/.exec(file)
+    if (tsMatch) parser = tsMatch[1]
+    else {
+      /* eslint-disable @typescript-eslint/no-var-requires */
+      const { dependencies, devDependencies } = require(`${findRoot(
+        file
+      )}/package.json`)
+      /* eslint-enable @typescript-eslint/no-var-requires */
+      const deps = { ...dependencies, ...devDependencies }
+      if ('@babel/core' in deps) parser = 'babel'
+    }
+  }
+
+  const j = parser ? jscodeshift.withParser(parser) : jscodeshift
+
   const newCode = transform(
     { path: file, source: code },
     {
-      j: jscodeshift,
-      jscodeshift,
-      stats: (name: string, quantity?: number | undefined): void => {
+      j,
+      jscodeshift: j,
+      stats: (): void => {
         // noop
       },
-      report: (msg: string): void => {
+      report: (): void => {
         // noop
       },
     },
