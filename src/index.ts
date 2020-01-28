bfb0b24731d9308898584c9ef55eba2633de1c00
Andy Edwards <jedwards@fastmail.com>
2020-01-28 15:22:05 -0600

    fix: move logic into jscodeshift-choose-parser package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as vscode from 'vscode'
-import * as path from 'path'
 import jscodeshift, { Transform, Options } from 'jscodeshift'
-import { ASTNode } from 'recast'
-import resolve from 'resolve'
+import chooseJSCodeshiftParser from 'jscodeshift-choose-parser'
 
 export default async function applyTransform(
   transform: Transform,
@@ -18,40 +16,7 @@ export default async function applyTransform(
   const selectionEnd = editor.document.offsetAt(editor.selection.end)
 
   let parser = options ? options.parser : null
-  if (!parser) {
-    /* eslint-disable @typescript-eslint/no-var-requires */
-    const cwd = path.dirname(file)
-    const tsMatch = /\.(tsx?)$/.exec(file)
-    if (tsMatch) {
-      try {
-        // if @babel/preset-typescript is found, try to parse with @babel/core instead of ts/x.
-        resolve.sync('@babel/preset-typescript', { basedir: cwd })
-      } catch (error) {
-        parser = tsMatch[1]
-      }
-    }
-    if (!parser) {
-      try {
-        const babel = require(resolve.sync('@babel/core', { basedir: cwd }))
-        parser = {
-          parse(code: string): ASTNode {
-            return babel.parseSync(code, {
-              cwd,
-              filename: file,
-              // without this, babel won't search upward for a config file
-              rootMode: 'upward-optional',
-              // without this, jscodeshift would try to use esprima to tokenize the file,
-              // which may not work
-              parserOpts: { tokens: true },
-            })
-          },
-        }
-      } catch (error) {
-        // ignore
-      }
-    }
-    /* eslint-enable @typescript-eslint/no-var-requires */
-  }
+  if (!parser) parser = chooseJSCodeshiftParser(file)
 
   const j = parser ? jscodeshift.withParser(parser) : jscodeshift
 
